0f7df3972265036f50156e557ccda0126fc48070
mosolungo27 <marco.osnaya.rojas@gmail.com>
2023-10-05 16:40:58 -0600

    cambio de response

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Response
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from bson import ObjectId
 from passlib.hash import sha256_crypt
 from starlette.status import HTTP_204_NO_CONTENT, HTTP_201_CREATED
@@ -18,11 +20,15 @@ async def create_user(user_request : User):
     new_user = dict(user_request)
     new_user["password"] = sha256_crypt.encrypt(new_user["password"])
     id_save = conn.pruebas.users.insert_one(new_user).inserted_id
-    return Response(status_code=HTTP_201_CREATED)
+    user_request = user_entity(conn.pruebas.users.find_one({"_id":ObjectId(id_save)}))
+    user_response = jsonable_encoder(user_request)
+    return JSONResponse(status_code=HTTP_201_CREATED,content=user_response)
 
 @routes.get("/users/{id}", response_model=list[User], tags=["users"])
 async def find_user(id_request: str):
-    return user_entity(conn.pruebas.users.find_one({"_id":ObjectId(id_request)}))
+    find_user = user_entity(conn.pruebas.users.find_one({"_id":ObjectId(id_request)}))
+    user_response = jsonable_encoder(find_user)
+    return JSONResponse(content=user_response)
 
 @routes.put("/users/{id}", response_model=list[User], tags=["users"])
 async def update_user(id_request: str, user_request: User):
@@ -31,7 +37,9 @@ async def update_user(id_request: str, user_request: User):
     conn.pruebas.users.find_one_and_update(
         {"_id": ObjectId(id_request)},{"$set":dict(new_user)}
     )
-    return user_entity(conn.pruebas.users.find_one({"_id":ObjectId(id_request)}))
+    find_user = user_entity(conn.pruebas.users.find_one({"_id":ObjectId(id_request)}))
+    user_response = jsonable_encoder(find_user)
+    return JSONResponse(content=user_response)
 
 @routes.delete("/users/{id}", response_model=list[User], tags=["users"])
 async def delete_user(id_request: str):
